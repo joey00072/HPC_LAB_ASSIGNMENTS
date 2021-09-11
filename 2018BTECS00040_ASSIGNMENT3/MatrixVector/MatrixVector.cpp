a8517416e01526df13163bc6e38fd6bb3e56f846
joey00072 <shubhamchoudhari00072@gmail.com>
2021-09-11 16:41:50 +0530

    fixes

--- 2018BTECS00040_ASSIGNMENT3/MatrixVector/MatrixVector.cpp
+++ 2018BTECS00040_ASSIGNMENT3/MatrixVector/MatrixVector.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<stdio.h>
 #include<omp.h>
+#define NUM 4
 
 using namespace std;
 
@@ -9,43 +10,44 @@ int main()
 	int size=0;
 	printf("Enter size of matrix\n");
 	scanf("%d", &size);
-	omp_set_num_threads(10);
+	omp_set_num_threads(NUM);
 	int* matrix = new int[size*size];
 	int* vector = new int[size];
 	int* resVector = new int[size];
-	#pragma omp for
-	for(int i=0; i<size; i++)
-	{
-		for(int j=0; j<size; j++)
-			*(matrix+i*size+j) = 2;
-	}
-	#pragma omp for
-	for(int i=0; i<size; i++)
-	{
-		*(vector+i)= 3;
-	}
-	int temp=0;
 	double time = omp_get_wtime();
-	#pragma omp for
-	for(int i=0; i<size; i++)
+	#pragma omp parallel
 	{
-		for(int j=0; j<size; j++)
+		#pragma omp for
+		for(int i=0; i<size; i++)
 		{
-			temp=0;
-			for(int k=0; k<size; k++)
-			{
-				temp += *(matrix+j*size+k) * *(vector+k);
-			}
+			for(int j=0; j<size; j++)
+				*(matrix+i*size+j) = 2;
 		}
-		printf("temp is %d\n", temp);
-		*(resVector+i) = temp;
+		#pragma omp for
+		for(int i=0; i<size; i++)
+		{
+			*(vector+i)= 3;
+		}
+		int temp=0;
+		#pragma omp for
+		for(int i=0; i<size; i++)
+		{
+			for(int j=0; j<size; j++)
+			{
+				temp=0;
+				for(int k=0; k<size; k++)
+				{
+					temp += *(matrix+j*size+k) * *(vector+k);
+				}
+			}
+			*(resVector+i) = temp;
+		}
 	}
-	
+	printf("\nFor size = %d\nDone in %f seconds\n", size, omp_get_wtime()-time);
+
 	for(int i=0; i<size; i++)
 	{
 		printf(" %d ", resVector[i]);
 	}
-	
-	printf("\nFor size = %d\nDone in %f seconds\n", size, omp_get_wtime()-time);
 	return 0;
 }
